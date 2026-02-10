e24ec6b7ea12febfd5ff9022bb313c089af14d50
nickolaa <nickolaa@gmail.com>
2026-02-10 23:12:24 +0300

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import AppRoutes from './routes';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
