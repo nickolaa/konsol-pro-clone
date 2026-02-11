fc047219b57c29d3aca56c2baabc0acc6bc7a2b0
nickolaa <nickolaa@gmail.com>
2026-02-11 22:33:42 +0300

    Add root URL redirect to API schema

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('', redirect, {'url': '/api/schema/'}, name='root'),
+
     
     # API Schema
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
