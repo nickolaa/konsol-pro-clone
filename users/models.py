71eaa1750bcd503f6acb6bf02d1f3f4cbf8b1f01
nickolaa <nickolaa@gmail.com>
2026-02-11 22:04:22 +0300

    Fix User model groups and permissions related_name conflicts

--- users/models.py
+++ users/models.py
@@ -7,6 +7,23 @@ class User(AbstractUser):
     is_freelancer = models.BooleanField(default=False)
     is_employer = models.BooleanField(default=False)
     
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='группы',
+        blank=True,
+        help_text='Группы, к которым принадлежит пользователь',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='права пользователя',
+        blank=True,
+        help_text='Специфические права этого пользователя',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+    
     class Meta:
         db_table = 'users'
         verbose_name = 'Пользователь'
