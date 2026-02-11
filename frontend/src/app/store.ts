62854bea75c9b3993b9242ec6a0cbe9d821e4c60
nickolaa <nickolaa@gmail.com>
2026-02-11 19:13:42 +0300

    Update store.tsRegister payments, reviews, and documents reducers in Redux store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import tasksReducer from '../features/tasks/tasksSlice';
+import paymentsReducer from '../features/payments/paymentsSlice';
+import reviewsReducer from '../features/reviews/reviewsSlice';
+import documentsReducer from '../features/documents/documentsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     tasks: tasksReducer,
+    payments: paymentsReducer,
+    reviews: reviewsReducer,
+    documents: documentsReducer,
   },
 });
 
